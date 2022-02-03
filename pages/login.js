6b889c3a50eaa4e6cddaa1b9de27c244163df849
Gatik <gatik.rajput@symbtechnologies.com>
2022-02-03 16:08:46 +0530

    add

---
+++ pages/login.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {APP_NAME} from "../utils/constant";
+import AuthLayoutComponent from "../component/auth-layout/auth-layout";
+import Login from '../component/login/login';
+
+export default function Home() {
+
+  return (
+    <div>
+      <Head>
+        <title>{APP_NAME} - login</title>
+        <meta name="description" content="Trusted Brands. Better Health." />
+        <link rel="icon" href="/fitcart.ico" />
+        </Head>
+
+      <main>
+      <AuthLayoutComponent image={"/images/login.png"}>
+        <div className='row justify-content-center margin-login '>
+          <div className='col-8 p-0 '>
+            <Login/>
+          </div>
+        </div>
+      </AuthLayoutComponent>
+      </main>
+
+      
+    </div>
+  )
+}

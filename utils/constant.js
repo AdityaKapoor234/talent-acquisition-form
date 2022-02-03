fabd0de07dcea3d439345efa1bdf6e13457902cc
Gatik <gatik.rajput@symbtechnologies.com>
2022-02-03 16:05:39 +0530

    add

---
+++ utils/constant.js
@@ -0,0 +1,8 @@
+import getConfig from 'next/config'
+const {publicRuntimeConfig} = getConfig()
+const {API_BASE_URL_SPACE_SERVICE, API_BASE_URL_USER_SERVICE,API_BASE_URL_PROPOSAL_SERVICE, API_BASE_URL_AUTH_SERVICE, WEBSITE_NAME, WEBSITE_TAGLINE, WEBSITE_IMAGE_URL,WEBSITE_URL} = publicRuntimeConfig
+
+export const APP_NAME = WEBSITE_NAME
+export const APP_TAGLINE = WEBSITE_TAGLINE
+export const APP_IMAGE = WEBSITE_IMAGE_URL
+export const APP_URL = WEBSITE_URL

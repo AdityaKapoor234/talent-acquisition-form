d28f8156e4dd010a50e470a6b9cd93ed1c21dc72
Gatik <gatik.rajput@symbtechnologies.com>
2022-04-01 10:53:32 +0530

    issue fixed

--- utils/constant.js
+++ utils/constant.js
@@ -66,7 +66,7 @@ export const ORDERSLIST = `http://65.1.17.188:5002/manage/order/?page={{page}}&q
 export const GET_ORDER_DETAILS = `http://65.1.17.188:5002/manage/order/{{id}}`;
 
 //PRODUCT
-export const GET_INGREDIENT = `http://65.1.17.188:5001/ingredient/`;
+export const GET_INGREDIENT = `http://65.1.17.188:5001/manage/ingredient/all`;
 export const ADD_SUPPLEMENT = `http://65.1.17.188:5001/manage/product/{{id}}/supplement-facts`;
 export const CONTENTLIST = `http://65.1.17.188:5001/manage/product/{{id}}/content`; 
 export const CONTENT_LIST_EDIT = `http://65.1.17.188:5001/manage/product/{{id}}/content`

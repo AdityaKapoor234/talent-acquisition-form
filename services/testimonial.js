96e044844f5cf35b2419d355bd7a940fc7a05c8f
Aditya Kapoor <aditya.kapoor@symbtechnologies.com>
2022-06-12 23:35:29 +0530

    Testimonial -> Testimonial, Category, Gallery

---
+++ services/testimonial.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { TESTIMONIAL_LIST, TESTIMONIAL_VIEW, TESTIMONIAL_EDIT, TESTIMONIAL_CREATE, TESTIMONIAL_DELETE } from "../utils/constant";
+import cookie from "js-cookie";
+
+export class TestimonialAPI {
+
+    static testimonialList(page, search) {
+        const token = cookie.get('access_token_admin');
+        const httpOptions = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token} `
+            }
+        };
+        return axios.get(`${TESTIMONIAL_LIST}`.replace('{{page}}', page).replace('{{search}}', search), httpOptions)
+    }
+
+    static testimonialViewDetails(id) {
+        const token = cookie.get('access_token_admin');
+        const httpOptions = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token} `
+            }
+        };
+        return axios.get(`${TESTIMONIAL_VIEW}`.replace('{{id}}', id), httpOptions)
+    }
+
+    static testimonialListEDIT(id, data) {
+        const token = cookie.get('access_token_admin');
+        const httpOptions = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token} `
+            }
+        };
+        return axios.post(`${TESTIMONIAL_EDIT}`.replace('{{id}}', id), data, httpOptions)
+    }
+
+    static testimonialCreate(data) {
+        const token = cookie.get('access_token_admin');
+        const httpOptions = {
+            headers: {
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${token} `          
+            }
+        };
+        return axios.post(`${TESTIMONIAL_CREATE}`,data,httpOptions)
+    }
+
+    static testimonialDelete(id,data) {
+        const  token = cookie.get('access_token_admin');
+        const httpOptions = {
+            headers: {
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${token} `          
+            }
+        };
+        return axios.post(`${TESTIMONIAL_DELETE}`.replace('{{id}}', id),data,httpOptions)
+    }
+}
+export default TestimonialAPI;

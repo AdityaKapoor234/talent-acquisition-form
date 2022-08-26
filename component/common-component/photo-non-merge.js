5167a86dab1af7ba788d86ee2775c5e23972ae97
Aditya Kapoor <aditya.kapoor@symbtechnologies.com>
2022-08-26 16:36:46 +0530

    Banner Image Uploads

---
+++ component/common-component/photo-non-merge.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import ImageIcon from "@mui/icons-material/Image";
+import cookie from "js-cookie";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import InfoIcon from "@mui/icons-material/Info";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+
+export default function photo({
+  label,
+  accept,
+  mode,
+  img,
+  name,
+  setUrl,
+  value,
+  urlLink,
+  size
+}) {
+  const [image, setImage] = useState(img ? img : "");
+  const [isLoader, setIsLoader] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  function uploadFile({ target: { files } }) {
+    if (files?.length > 0) {
+      const formData = new FormData();
+      formData.append("media", files[0]);
+      uploadfile(urlLink, formData, name);
+    }
+  }
+  const uploadfile = (url, image, names) => {
+    setIsLoader(true);
+    const token = cookie.get("access_token_admin");
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .put(url, image, headers)
+      .then((response) => {
+        setIsLoader(false);
+        setImage(response.data.data?.url);
+        setUrl(names, response.data.data?.url);
+      })
+      .catch((error) => {
+        setIsLoader(false);
+        toast.error(error);
+      });
+  };
+  useEffect(() => {
+    setImage(img ? img : "");
+  }, [img]);
+
+  return (
+    <div className="photo">
+      <Backdrop
+        open={isLoader}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <div className="label">
+        {label}
+        <span className="mandatory-star">*</span>
+        {size && <InfoIcon
+          className="info-icon"
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+        />}
+
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography sx={{ p: 1 }}>{size}</Typography>
+        </Popover>
+      </div>
+      <div className="photo-box">
+        <div className="photo-image" style={{ background: `url(${image})` }}>
+          {image === "" && mode === "edit" ? (
+            <>
+              <ImageIcon className="image-icon" />
+              <label>No file Chosen</label>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+        {mode === "edit" && (
+          <label for={value} className="file">
+            Choose File
+          </label>
+        )}
+      </div>
+      <input
+        id={value}
+        type="file"
+        name={name}
+        accept={accept}
+        onChange={uploadFile}
+      />
+    </div>
+  );
+}

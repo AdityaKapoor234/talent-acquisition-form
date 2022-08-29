352cd93085b35711dbdc2ef1ecbbe97de7fcf8c1
Aditya Kapoor <aditya.kapoor@symbtechnologies.com>
2022-08-29 16:41:21 +0530

    Image Cropper Fix

--- component/catalog/category/category-create.js
+++ component/catalog/category/category-create.js
@@ -10,7 +10,7 @@ import ArticleEditor from "../../common-component/text-editer";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Photo from "../../common-component/photo";
+import Photo from "../../common-component/photo-non-merge";
 import CategoryApi from "../../../services/category";
 import {PRODUCT_SERVICE} from "../../../utils/constant";
 
